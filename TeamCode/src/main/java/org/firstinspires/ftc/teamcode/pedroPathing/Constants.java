98184cf85968f601c3ba499a1a9a9adcbd8ed4cd
__dnl_ <danillopedro2020@gmail.com>
2025-11-22 17:37:56 -0300

    Update Constants.java
    Refactor follower and drive constants, add localizer constants.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -7,19 +7,10 @@ import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
-
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
-
-    public static Follower createFollower(HardwareMap hardwareMap) {
-        return new FollowerBuilder(followerConstants, hardwareMap)
-                .pathConstraints(pathConstraints)
-                .build();
-    }
     public static FollowerConstants followerConstants = new FollowerConstants()
         .mass(15);
 
-    public static MecanumConstants driveConstants = new MecanumConstants()
+     public static MecanumConstants driveConstants = new MecanumConstants()
         .maxPower(1)
         .rightFrontMotorName("MDF")
         .rightRearMotorName("MDT")
@@ -30,10 +21,23 @@ public class Constants {
         .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
         .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
 
+    public static TwoWheelConstants localizerConstants = new TwoWheelConstants()
+        .forwardEncoder_HardwareMapName("leftFront") //mudar
+        .strafeEncoder_HardwareMapName("rightRear") //mudar
+        .IMU_HardwareMapName("imu") //conferir
+        .IMU_Orientation(
+            new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT
+            )
+        );
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
-                .mecanumDrivetrain(driveConstants)
+                .mecanumDriveTrain(driveConstants)
                 .build();
     }
 }

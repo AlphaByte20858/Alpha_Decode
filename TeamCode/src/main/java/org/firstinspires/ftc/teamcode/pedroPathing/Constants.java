5922f1a50e8457b276bbca0a506cf07a789d506e
__dnl_ <danillopedro2020@gmail.com>
2025-11-22 18:40:45 -0300

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -33,8 +33,8 @@ public class Constants {
         .strafeTicksToInches(multiplier)
         .IMU_Orientation(
             new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.LEFT
+                RevHubOrientationOnRobot.LogoFacingDirection.right,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP
             )
         );
 
